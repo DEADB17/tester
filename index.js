44d2c4d16ea3bb1a16128a5f7f4a6a1f767d18f8
DEADB17 <DEADB17@gmail.com>
2020-05-29 11:36:56 -0400

    Simplify parent popping condition

--- index.js
+++ index.js
@@ -93,9 +93,7 @@ export function run(callback, tests) {
 
   const upd = (status, item) => {
     callback({ item, parents, status, time: Date.now() });
-    if (status !== start && item === parents[parents.length - 1]) {
-      parents.pop();
-    }
+    if (item === parents[parents.length - 1]) parents.pop();
   };
 
   const stack = [...tests];
